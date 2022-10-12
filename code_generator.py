c0037e54d1348676e2009f6421b71a0130d78ad8
Berkay <101340187+Destiqua@users.noreply.github.com>
2022-10-12 23:01:01 +0300

    Add files via upload

---
+++ code_generator.py
@@ -0,0 +1,8 @@
+import random
+x = random.randrange(10)
+y = random.randrange(10)
+z = random.randrange(10)
+a = random.randrange(10)
+s = random.randrange(10)
+d = random.randrange(10)
+print(x,y,z,a,s,d)
