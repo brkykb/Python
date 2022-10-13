6030327ac15355c0b2cdd4b7b0c99395a50d2aac
Berkay <101340187+Destiqua@users.noreply.github.com>
2022-10-13 12:36:34 +0300

    Delete code_generator.py

--- code_generator.py
+++
@@ -1,8 +0,0 @@
-import random
-x = random.randrange(10)
-y = random.randrange(10)
-z = random.randrange(10)
-a = random.randrange(10)
-s = random.randrange(10)
-d = random.randrange(10)
-print(x,y,z,a,s,d)
